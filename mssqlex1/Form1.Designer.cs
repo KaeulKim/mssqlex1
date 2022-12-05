55ff87cea14d41744ef9b11475dead0f0ac804d9
rkdmf8937 <rkdmf8937@gmail.com>
2022-12-05 11:09:29 +0900

    초기 커밋.

---
+++ mssqlex1/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace mssqlex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.testDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.txt_age = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet1BindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(105, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 226);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(91, 369);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 1;
+            this.btn_Delete.Text = "삭제";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.Location = new System.Drawing.Point(256, 369);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(75, 23);
+            this.btn_Create.TabIndex = 2;
+            this.btn_Create.Text = "신규";
+            this.btn_Create.UseVisualStyleBackColor = true;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(417, 369);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 3;
+            this.btn_Save.Text = "저장";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // txt_age
+            // 
+            this.txt_age.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_age.Location = new System.Drawing.Point(100, 0);
+            this.txt_age.Name = "txt_age";
+            this.txt_age.Size = new System.Drawing.Size(100, 21);
+            this.txt_age.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_age);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(394, 490);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 23);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SandyBrown;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("굴림", 13F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Age";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txt_name);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(394, 464);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 23);
+            this.panel2.TabIndex = 6;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_name.Location = new System.Drawing.Point(100, 0);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 21);
+            this.txt_name.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SandyBrown;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Font = new System.Drawing.Font("굴림", 13F);
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txt_id);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(394, 438);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 23);
+            this.panel3.TabIndex = 7;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_id.Location = new System.Drawing.Point(100, 0);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 21);
+            this.txt_id.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.SandyBrown;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Font = new System.Drawing.Font("굴림", 13F);
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Location = new System.Drawing.Point(546, 369);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_Refresh.TabIndex = 8;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(701, 57);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(494, 300);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1494, 634);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Create);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet1BindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource testDataSet1BindingSource;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.TextBox txt_age;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ mssqlex1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace mssqlex1
+{
+    public partial class Form1 : Form
+    {
+        private DataTable data_table = null;
+        private int[] numers = { 1, 2, 3, 4, 5 };
+        string mode = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadDbTableUsingAdapter();
+        }
+
+        public void LoadTableUsingReader()
+        {
+            try
+            {
+                lock (DBHelper.DBConn)
+                {
+                    if (!DBHelper.IsDBConnected)
+                    {
+                        MessageBox.Show("DB 연결을 확인하세요");
+                        return;
+                    }
+                    else
+                    {                     
+                        SqlDataAdapter adapter = new SqlDataAdapter("Select * FROM Table1", DBHelper.DBConn);
+                        data_table = new DataTable();
+                        try
+                        {
+                            adapter.Fill(data_table);
+                           
+
+                            dataGridView1.DataSource = data_table;
+                            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message, "Datagridveiw_Load_Error");
+                        }
+                        DBHelper.Close();
+                    }
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message, "Datagridveiw_Load_Error");
+            }
+        }
+
+        public void LoadDbTableUsingAdapter()
+        {
+            try
+            {
+                lock (DBHelper.DBConn)
+                {
+                    if (!DBHelper.IsDBConnected)
+                    {
+                        MessageBox.Show("DB 연결을 확인하세요");
+                        return;
+                    }
+                    else
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter("Select * FROM Table1", DBHelper.DBConn);
+                        data_table = new DataTable();
+                        try
+                        {
+                            adapter.Fill(data_table);
+
+                            chart1.DataSource = data_table;
+                            chart1.Series[0].XValueMember = "Name";
+                            chart1.Series[0].YValueMembers = "Age";
+                            chart1.DataBind();
+
+                            dataGridView1.DataSource = data_table;
+                            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message, "Datagridveiw_Load_Error");
+                        }
+                        DBHelper.Close();
+                    }
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message, "Datagridveiw_Load_Error");
+            }
+        }
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count != 0)
+                {
+                    foreach (DataGridViewRow item in dataGridView1.SelectedRows)
+                    {
+
+                        SqlCommand sqlCommand = new SqlCommand();
+                        sqlCommand.Connection = DBHelper.DBConn;
+                        sqlCommand.CommandText = string.Format("Delete Table1 Where ID = {0}", item.Cells[2].Value);
+                        sqlCommand.ExecuteNonQuery();
+
+                        LoadDbTableUsingAdapter();
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("선택해주세요.");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = DBHelper.DBConn;
+                sqlCommand.CommandText = string.Format("insert into Table1(Name,Age,ID) values('{0}',{1},'{2}')", txt_name.Text, txt_age.Text, txt_id.Text);
+                sqlCommand.ExecuteNonQuery();
+
+                LoadDbTableUsingAdapter();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //sqlCommand.ex
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            LoadDbTableUsingAdapter();
+        }
+    }
+
+    public class DBHelper
+    {
+        private static SqlConnection conn = null;
+        public static string DBConnString { get; private set; }
+
+        public static bool bDBConnCheck = false;
+
+        private static int errorBoxCount = 0;
+
+        public DBHelper() { }
+
+        public static SqlConnection DBConn
+        {
+            get
+            {
+                if(!ConnectToDB())
+                {
+                    return null;
+                }
+                return conn;
+            }
+        }
+
+        public static bool IsDBConnected
+        {
+            get
+            {
+                if(conn.State != ConnectionState.Open)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private static bool ConnectToDB()
+        {
+            if (conn == null)
+            {
+                DBConnString = string.Format("Data Source = 127.0.0.1; Initial Catalog = Test; User ID = User1; Password = 1234");
+
+                conn = new SqlConnection(DBConnString);
+            }
+
+            try
+            {
+                if(!IsDBConnected)
+                {
+                    conn.Open();
+                    if(conn.State == ConnectionState.Open)
+                    {
+                        bDBConnCheck = true;
+                    }
+                    else
+                    {
+                        bDBConnCheck = false;
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                errorBoxCount++;
+                if(errorBoxCount == 1)
+                {
+                    MessageBox.Show(e.Message, "DBHelper - ConnectToDB()");
+                }
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Close()
+        {
+            if(IsDBConnected)
+            {
+                DBConn.Close();
+            }
+        }
+    }
+}
